b2eb4f9109b3973bb2fb7611bfbcee40948ccfe5
Ebowlee <ebowlee@outlook.com>
2025-06-19 12:21:20 +0800

    v2.1.0

--- src/UniverseSelection.py
+++ src/UniverseSelection.py
@@ -1,7 +1,6 @@
 # region imports
 from AlgorithmImports import *
 from QuantConnect.Algorithm.Framework.Selection import FineFundamentalUniverseSelectionModel
-from datetime import timedelta
 # endregion
 
 class MyUniverseSelectionModel(FineFundamentalUniverseSelectionModel):
@@ -19,16 +18,24 @@ class MyUniverseSelectionModel(FineFundamentalUniverseSelectionModel):
         self.min_roe = 0.05
         self.max_debt_to_assets = 0.5
         self.max_leverage_ratio = 4
+        self.selection_needed = False 
 
-        self.fine_selection_interval = timedelta(days=10)   # 精选周期
-        self.last_fine_selection_date = None                # 上次精选日期
-        self.last_fine_selected_symbols = []                # 上次精选结果
-        self.fine_selection_count = 0                       # 精选次数
+        self.last_fine_selected_symbols = []                            # 上次精选结果
+        self.fine_selection_count = 0                                   # 精选次数
 
         algorithm.Debug("[UniverseSelection] 初始化完成")
 
         # 初始化父类并传递自定义的粗选和精选方法
         super().__init__(self._select_coarse, self._select_fine)
+
+
+
+    def TriggerSelection(self):
+        """
+        外部调用的触发接口：用于在 main.py 中通过 Schedule.On 控制
+        """
+        self.selection_needed = True
+        self.algorithm.Debug("[UniverseSelection] 已收到主控调度，准备执行下一轮选股")
 
 
 
@@ -39,7 +46,6 @@ class MyUniverseSelectionModel(FineFundamentalUniverseSelectionModel):
         filtered = [x for x in coarse if x.HasFundamentalData and x.Price > self.min_price and x.DollarVolume > self.min_volume]
         ipo_filtered = [x for x in filtered if x.SecurityReference.IPODate is not None and (self.algorithm.Time - x.SecurityReference.IPODate).days > self.min_ipo_days]
         coarse_selected = [x.Symbol for x in sorted(ipo_filtered, key=lambda x: x.Symbol.Value)]
-        # self.algorithm.Debug(f"[_select_coarse] 粗选阶段完成, 共选出: {len(coarse_selected)}, 部分结果展示: {[x.Value for x in coarse_selected[:10]]}")
         return coarse_selected
 
 
@@ -48,8 +54,8 @@ class MyUniverseSelectionModel(FineFundamentalUniverseSelectionModel):
         """
         细选阶段，主要是依据行业和财报信息
         """
-        current_date = self.algorithm.Time.date()
-        if self.last_fine_selection_date is None or (current_date - self.last_fine_selection_date).days >= self.fine_selection_interval.days:
+        if self.selection_needed:
+            self.selection_needed = False 
             self.fine_selection_count += 1
             self.algorithm.Debug(f"=============================开始第【{self.fine_selection_count}】次选股=============================") 
 
@@ -72,7 +78,6 @@ class MyUniverseSelectionModel(FineFundamentalUniverseSelectionModel):
                 candidates = [x for x in fine if x.AssetClassification.MorningstarSectorCode == code]
                 sector_candidates[name] = self._num_of_candidates(candidates)
 
-            # 用循环选择每个行业的最终股票
             for selected_fine_list in sector_candidates.values():
                 all_sectors_selected_fine.extend(selected_fine_list)
 
@@ -93,13 +98,8 @@ class MyUniverseSelectionModel(FineFundamentalUniverseSelectionModel):
 
             final_selected_symbols = [x.Symbol for x in fine_after_financial_filters]
             self.last_fine_selected_symbols = final_selected_symbols
-            self.last_fine_selection_date = current_date
             self.algorithm.Debug(f"[UniverseSelection] -- [_select_fine] 精选阶段完成, 共选出: {len(final_selected_symbols)}, 部分结果展示: {[x.Value for x in final_selected_symbols[:10]]}")
             return final_selected_symbols
-
-        else:
-            self.algorithm.Debug(f"[UniverseSelection] -- [_select_fine] 距离下次选股还剩【{self.fine_selection_interval.days - (current_date - self.last_fine_selection_date).days}】天")
-            return self.last_fine_selected_symbols
 
 
     def _num_of_candidates(self, sectorCandidates):
@@ -107,7 +107,7 @@ class MyUniverseSelectionModel(FineFundamentalUniverseSelectionModel):
         根据行业筛选出符合条件的股票，按市值降序排列
         """
        # 返回市值最高的前 self.numOfCandidates 个股票
-        filtered = [x for x in sectorCandidates if x.MarketCap is not None and x.MarketCap > 0]
+        filtered = [x for x in sectorCandidates if x.MarketCap is not None and x.MarketCap > self.min_market_cap]
         sorted_candidates = sorted(filtered, key=lambda x: x.MarketCap, reverse=True)
         return sorted_candidates[:self.numOfCandidates]
  
